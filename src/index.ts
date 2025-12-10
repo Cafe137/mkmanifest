82e42d4df7f879b44ea43674f6ae5c7d68897d4e
Cafe137 <xcafe137@gmail.com>
2025-12-10 19:09:06 +0100

    feat: implement mantaray construction

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import { log } from 'console'
+#!/usr/bin/env node
 
-log('it works')
+import * as core from '@actions/core'
+import { MantarayNode, MerkleTree, NULL_ADDRESS } from '@ethersphere/bee-js'
+import { Arrays, Binary } from 'cafe-utility'
+import { argv, env } from 'process'
+
+const payload = Arrays.requireStringArgument(argv, 'payload', env, 'MKMANIFEST_PAYLOAD')
+const filename = Arrays.requireStringArgument(argv, 'filename', env, 'MKMANIFEST_FILENAME')
+const contentType = Arrays.requireStringArgument(argv, 'content-type', env, 'MKMANIFEST_CONTENT_TYPE')
+
+main()
+
+async function main() {
+    const cac = await MerkleTree.root(new TextEncoder().encode(payload))
+    const mantaray = new MantarayNode()
+    mantaray.addFork('/', NULL_ADDRESS, {
+        'website-index-document': filename
+    })
+    mantaray.addFork(filename, cac.hash(), {
+        'Content-Type': contentType,
+        Filename: filename
+    })
+    const address = await mantaray.calculateSelfAddress()
+    core.setOutput('mkmanifest_result_address', address.toHex())
+    core.setOutput('mkmanifest_result_payload', Binary.uint8ArrayToHex(await mantaray.marshal()))
+}
